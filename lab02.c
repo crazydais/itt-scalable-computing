ed8b811f7dda0b24357a3fb7c1eb122f8afc2876
davenolan1@gmail.com <davenolan1@gmail.com@c95b5d17-0367-8b09-ea2a-20cfb13ed904>
2013-02-06 21:23:04 +0000

    06.02.2013_v.01

---
+++ lab02.c
@@ -0,0 +1,28 @@
+/*	Part 4:
+	--------
+To build a program to compute the arithmetic mean and variance (lecture) of an array of integers in C/C++
+
+1. First, we will consider how to allocate space to an array (lecture)
+2. We need to set the values of the array once the space has been allocated (lecture).
+3. Look at how to design a function in C/C++ (lecture)
+4. To design the appropriate function parameter list, we need to look at the objective of the function:
+(a) calcAvg 
+(b) calcVar
+5. Take some time to design the function "prototype"
+6. Write the calcAvg and calcVar
+*/
+
+//	Week 2, Lab 02
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+
+	//	int arrayOfNumber[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+
+
+}
