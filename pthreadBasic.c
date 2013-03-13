9e5a4b88c6b07502dd5b1c8eaaf800717d76a0e8
davenolan1@gmail.com <davenolan1@gmail.com@c95b5d17-0367-8b09-ea2a-20cfb13ed904>
2013-03-13 23:35:33 +0000

    13/03/13 labs

--- pthreadBasic.c
+++ pthreadBasic.c
@@ -1,19 +1,23 @@
 #include <pthread.h>
 #include <stdio.h>
+
 #define NUM_THREADS     5
 
 void *PrintHello(void *threadid);
 
 void *PrintHello(void *threadid)
 {
-	printf("\nHello World! It's me, thread #%d!",(int)threadid);
+	int i;
+
+		printf("\nHello World! It's me, thread #%ld!",(long)threadid);	
 }
 
 int main (int argc, char *argv[])
 {
    pthread_t threads[NUM_THREADS];
 
-   for(int t = 0; t < NUM_THREADS; t++){
+   for(long t = 0; t < NUM_THREADS; t++)
+   {
       int rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
    }
 
@@ -26,3 +30,17 @@ int main (int argc, char *argv[])
 
 
 //	void * => can  be passed to any data type.
+
+/*
+pthread_create (thread,attr,start_routine,arg)
+
+pthread_create arguments...
+
+thread:		An opaque, unique identifier for the new thread returned by the subroutine.
+attr: 		An opaque attribute object that may be used to set thread attributes. You can specify a thread attributes object, or NULL for the default values.
+start_routine: the C routine that the thread will execute once it is created.
+arg: 		A single argument that may be passed to start_routine. It must be passed by reference as a pointer cast of type void. NULL may be used if no argument is to be passed.
+
+The maximum number of threads that may be created by a process is implementation dependent. Programs that attempt to exceed the limit can fail or produce wrong results.
+
+*/
